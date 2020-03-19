aeb8341fdb9dbf0be672509eb7768c711998c391
jamievullo <jvullo@protonmail.com>
2020-03-19 15:07:25 -0400

    Edited about me.

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -9,13 +9,13 @@ export default function AboutPage(props) {
                 <Content>
 
                     <p>I'm a full-stack web developer based in Scranton, Pennsylvania. I love coding 
-                    and I am completely driven to create awe-inspiring, attention-getting, unique, 
+                    and am completely driven to create awe-inspiring, attention-getting, unique, 
                     and ingenious interactive web applications from scratch.</p>
 
                     
                     <p>As a software engineer, I get to create anything I can dream up and 
                     I do so almost every day. From an interactive augmented reality application
-                    all the way down to the simplest of informational websites. While being sure to maintain 
+                    down to the simplest of informational websites. While also being sure to maintain 
                     the best practices and affording the utmost care and attention. I love to 
                     push the envelope of innovation and strive to surpass ever evolving industry standards. </p>
                     <div style={{height: "70px"}}></div>
